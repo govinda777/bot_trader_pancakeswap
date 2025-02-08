9ab79ca0d599c4a48c4372a8a835d2bc1e4354e1
Govinda <govinda777@gmail.com>
2025-02-08 18:52:26 -0300

    up

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 
+import sys
 import pytest
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 import os
 from environment import EnvironmentSettings
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 @pytest.fixture(scope="session")
 def env_settings():
